2cf8f361b061d93e35d897021c9c6f1594d56d83
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2024-04-21 11:13:04 +0200

    start project configuration

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {createRoot} from "react-dom";
+
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
