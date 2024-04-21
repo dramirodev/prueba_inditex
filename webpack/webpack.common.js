8aad55b2f748e39fa131a54ea4c6c33be60c6ee9
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2024-04-21 14:44:04 +0200

    fix: chunk adding contenthash

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -34,7 +34,8 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, '..', 'build'),
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
+        clean: true
     },
     plugins: [
         new HtmlWebpackPlugin({
