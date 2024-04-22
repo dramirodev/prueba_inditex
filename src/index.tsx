8fe97947a2a4721aaa573124df00a116b33fa413
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2024-04-22 10:48:46 +0200

    feat: clean router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
-root.render(<App />);
+const root = createRoot(document.getElementById("root")!);
+root.render(<RouterProvider router={router} />);
