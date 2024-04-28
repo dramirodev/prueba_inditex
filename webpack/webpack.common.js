5cd41bf8f8b0331e93995950fbba054a82adb713
M. David Ramiro <dramirobarragan@gmail.com>
2024-04-28 22:31:52 +0200

    fix: prettier format

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,46 +1,46 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, '..', './src/index.tsx'),
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline',
-            },
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
         ],
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-    },
-    output: {
-        path: path.resolve(__dirname, '..', 'build'),
-        filename: '[name].[contenthash].js',
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', './public/index.html'),
-        }),
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
     ],
-    stats: 'errors-only',
-}
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "build"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./public/index.html"),
+    }),
+  ],
+  stats: "errors-only",
+};
