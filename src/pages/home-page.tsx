1428f11ae7ead267f5545c9c3097251e6dd5fa8c
M. David Ramiro <dramirobarragan@gmail.com>
2024-04-28 21:51:49 +0200

    feat: get Podcasts in homepage

---
+++ src/pages/home-page.tsx
@@ -0,0 +1,12 @@
+import { useGetPodcasts } from "../api/podcatsts/hooks/useGetPodcasts";
+
+export function HomePage() {
+  const { data } = useGetPodcasts();
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <code>{JSON.stringify(data, null, 2)}</code>
+    </div>
+  );
+}
