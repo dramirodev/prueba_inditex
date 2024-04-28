1d7c1773b600272e8f6e37b261e66d5c55ff1a01
M. David Ramiro <dramirobarragan@gmail.com>
2024-04-28 21:46:49 +0200

    feat: type useGetPodcasts.ts

--- src/api/podcatsts/hooks/useGetPodcasts.ts
+++ src/api/podcatsts/hooks/useGetPodcasts.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPodcasts } from "../podcasts";
+import { Podcasts } from "../../../types";
 
 export function useGetPodcasts() {
-  return useQuery({
+  return useQuery<Podcasts>({
     queryKey: ["podcasts"],
     queryFn: getPodcasts,
   });
