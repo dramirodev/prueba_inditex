7f0a4374e421bb2f69322c5d07c62636459391e1
M. David Ramiro <dramirobarragan@gmail.com>
2024-04-30 13:31:25 +0200

    feat: add styles to search bar

---
+++ src/components/search-bar/index.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SearchBar = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-content: flex-end;
+  padding: 0 20px 0 0;
+  margin-bottom: 20px;
+`;
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,13 +1,19 @@
 import { useGetPodcasts } from "../../api/podcatsts/hooks/useGetPodcasts";
 import { FeedContainer } from "../../ui";
 import { PodcastCard } from "../../components/card/PodcastCard";
+import { SearchBar } from "../../components/search-bar";
 
 export function HomePage() {
   const { data, setFilterTerm } = useGetPodcasts();
 
   return (
     <>
-      <input onChange={(e) => setFilterTerm(e.target.value)} />
+      <SearchBar>
+        <input
+          onChange={(e) => setFilterTerm(e.target.value)}
+          placeholder="Filter podcasts..."
+        />
+      </SearchBar>
       <FeedContainer>
         {data?.feed.entry.map((entry) => (
           <PodcastCard
