775e6437b07493e070fb78af8a79e9d5259ffe0f
M. David Ramiro <dramirobarragan@gmail.com>
2024-05-02 15:40:10 +0200

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { persistQueryClient } from "@tanstack/react-query-persist-client";
 import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { GlobalStyles } from "./ui/global-style";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       gcTime: 1000 * 60 * 60 * 24, // 24 hours
+      retry: 2,
     },
   },
 });
@@ -26,6 +28,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
+      <GlobalStyles />
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
